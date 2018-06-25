b3e98f8bbf826f71e3150c254aada5163e4e9ebe
piyachatel <37926469+piyachatel@users.noreply.github.com>
2018-06-25 16:02:26 +0700

    test

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def reply(replyToken, textList):
     LINE_API = 'https://api.line.me/v2/bot/message/reply'
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': WjghX90v2F34cxZzGr1yZxcHX5l6UA+DaW6svTh1ifUTZqXIweGMSS0kVTZwLB+II6Ej3kxMpbehs6V6Uxx4YKNN9v1yqklxgpDpp6fuT+WTuZyylt69cVMVr93lyGOUCZlu8eZxZm2MfBCqW+hp1wdB04t89/1O/w1cDnyilFU=
+        'Authorization': 'WjghX90v2F34cxZzGr1yZxcHX5l6UA+DaW6svTh1ifUTZqXIweGMSS0kVTZwLB+II6Ej3kxMpbehs6V6Uxx4YKNN9v1yqklxgpDpp6fuT+WTuZyylt69cVMVr93lyGOUCZlu8eZxZm2MfBCqW+hp1wdB04t89/1O/w1cDnyilFU='
     }
     msgs = []
     for text in textList:
