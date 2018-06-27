9aeb2edbb4345a08447e37b8f1e430c798c8f6a9
piyachatel <37926469+piyachatel@users.noreply.github.com>
2018-06-27 17:38:12 +0700

    7

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from googlefinance import getQuotes
 import json
 import requests
 app = Flask(__name__)
@@ -23,6 +24,11 @@ def callback():
   print("ผู้ใช้：",user)
   print('ทดสอบ') 
   sendText(user,'สวัสดีค่ะ') # ส่งข้อความ งง
+
+
+symbol = 'PTT'       
+print(json.dumps(getQuotes('SET:' + symbol), indent=2))
+
   return '',200
 
 
